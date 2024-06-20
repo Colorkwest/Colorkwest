c764024da1891bd931ade96f38d09e12eb563b8a
Jeffrey Souza <jeffrey.souza@colorkrew.com>
2024-06-20 23:14:45 +0900

    Create QuestBoard component

--- frontend/colorkwest-front/src/App.tsx
+++ frontend/colorkwest-front/src/App.tsx
@@ -1,27 +1,10 @@
-import { useGetQuestsQuestsGet, useGetUsersUsersGet } from './generated/api/default/default';
-import { QuestBoxComponent } from './components/QuestBoxComponent.tsx';
-import { QuestionBoxComponent } from './components/QuestionBoxComponent.tsx';
-import { UserProfileComponent } from './components/UserProfileComponent.tsx';
-import { NewQuestModalComponent } from './components/NewQuestComponent.tsx';
 import { useState } from 'react';
-import { StatDisplay } from './components/StatDisplay.tsx';
-import { Box, Tab, Tabs } from '@mui/material';
+import { Box } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { TabSelector } from './components/TabSelector.tsx';
+import { QuestBoard } from './components/QuestBoard.tsx';
 
 function App() {
-  const quests = useGetQuestsQuestsGet();
-  const users = useGetUsersUsersGet();
-  const [newQuestModalOpen, setNewQuestionModalOpen] = useState<boolean>(false);
-
-  const handleAnswer = (answer: string) => {
-    alert(answer);
-  };
-
-  const handleAccept = () => {
-    alert('accept');
-  };
-
   const [selectedTab, setSelectedTab] = useState(0);
 
   const theme = createTheme({
@@ -41,26 +24,7 @@ function App() {
         <h1>Colorkwest</h1>
       </Box>
       <TabSelector selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      {quests?.data?.map((item) => {
-        if (item.type == 1) {
-          return (
-            <div key={item.id}>
-              <QuestionBoxComponent quest={item} handleAnswer={handleAnswer} />
-            </div>
-          );
-        } else
-          return (
-            <div key={item.id}>
-              <QuestBoxComponent quest={item} handleAccept={handleAccept} />
-            </div>
-          );
-      })}
-      {users?.data && <UserProfileComponent user={users?.data?.[1]} />}
-      <button onClick={() => setNewQuestionModalOpen((x) => !x)}>++++++</button>
-      {newQuestModalOpen && <NewQuestModalComponent handleSubmit={handleSubmit} />}
-      <Box width={600} height={600}>
-        <StatDisplay str={10} cha={10} int={20} dex={40} />
-      </Box>
+      <QuestBoard selectedTab={selectedTab} />
     </ThemeProvider>
   );
 }
---
+++ frontend/colorkwest-front/src/components/QuestBoard.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { useGetQuestsQuestsGet, useGetUsersUsersGet } from '../generated/api/default/default';
+import { Box } from '@mui/material';
+import { StatDisplay } from './StatDisplay';
+import { NewQuestModalComponent } from './NewQuestComponent';
+import { UserProfileComponent } from './UserProfileComponent';
+import { QuestBoxComponent } from './QuestBoxComponent';
+import { QuestionBoxComponent } from './QuestionBoxComponent';
+
+type QuestBoardProps = {
+  selectedTab: number;
+};
+
+export function QuestBoard({ selectedTab }: QuestBoardProps) {
+  const quests = useGetQuestsQuestsGet();
+  const users = useGetUsersUsersGet();
+  const [newQuestModalOpen, setNewQuestionModalOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      {quests?.data?.map((item) => {
+        if (item.type == 1) {
+          return (
+            <div key={item.id}>
+              <QuestionBoxComponent quest={item} handleAnswer={() => {}} />
+            </div>
+          );
+        } else
+          return (
+            <div key={item.id}>
+              <QuestBoxComponent quest={item} handleAccept={() => {}} />
+            </div>
+          );
+      })}
+      {users?.data && <UserProfileComponent user={users?.data?.[1]} />}
+      <button onClick={() => setNewQuestionModalOpen((x) => !x)}>+</button>
+      {newQuestModalOpen && <NewQuestModalComponent handleSubmit={() => {}} />}
+      <Box width={600} height={600}>
+        <StatDisplay str={10} cha={10} int={20} dex={40} />
+      </Box>
+    </>
+  );
+}
